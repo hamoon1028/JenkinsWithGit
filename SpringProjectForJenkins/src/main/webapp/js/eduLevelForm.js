b579f6dd77f48037277319671f5eb642b27e5fba
hamoon1028 <mho1028@gmail.com>
2023-09-11 16:39:30 +0900

    체크

--- SpringProjectForJenkins/src/main/webapp/js/eduLevelForm.js
+++ SpringProjectForJenkins/src/main/webapp/js/eduLevelForm.js
@@ -1,18 +1,8 @@
 $(document).ready(function() {
    
-    var val= getEduLevelText(1);
-    document.getElementById("jsVal").innerHTML = val;
+   	var value= 1;
+    document.getElementById("jsVal").innerHTML = '대학원(박사)';
    
     });
 
-
-function getEduLevelText(value) {
-    switch(value) {
-        case '1': return '대학원(박사)';
-        case '2': return '대학원(석사)';
-        case '3': return '대학교(4년)';
-        case '4': return '대학(2,3년)';
-        case '5': return '고등학교';
-        default: return '';
-    }
-}
+    
